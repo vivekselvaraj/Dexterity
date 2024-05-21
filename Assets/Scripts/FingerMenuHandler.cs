ecf9a1271cbfedd3052e780791b4740549652c24
Selvaraj <vive@UMICH.EDU>
2024-05-21 00:52:37 -0400

    add video tutorial animatino

--- Assets/Scripts/FingerMenuHandler.cs
+++ Assets/Scripts/FingerMenuHandler.cs
@@ -10,6 +10,7 @@ public class FingerMenuHandler : MonoBehaviour
     public GameObject detail;
     public GameObject videoPlayer;
     public Transform cameraTransform;
+    public Transform handTransform;
     public float detailActivationDistance = 0.4f;
     public Animator animator;
 
@@ -25,17 +26,42 @@ public class FingerMenuHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 directionToCamera = (cameraTransform.position - handTransform.position).normalized;
+        float dotProduct = Mathf.Abs(Vector3.Dot(handTransform.forward, directionToCamera));
+        
+
         float distance = Vector3.Distance(detail.transform.position, cameraTransform.position);
-        if (distance < detailActivationDistance && !isDetailVisible)
+        if (distance < detailActivationDistance)
         {
             Debug.Log("Showing text......");
-            animator.SetBool("showText", true);
-            isDetailVisible = true;
+            if (!isDetailVisible)
+            {
+                isDetailVisible = true;
+                animator.SetBool("showText", true);
+            }
+            Debug.Log(dotProduct.ToString());
+            if (dotProduct < 0.4f)
+            {
+                animator.SetBool("showVideo", true);
+            } else
+            {
+                animator.SetBool("showVideo", false);
+            }
+          //  StartCoroutine(ShowVideoAfterDelay(3f)); // Start coroutine to show video after 3 second
+            
         }
-        else if (distance >= detailActivationDistance && isDetailVisible) {
+        else if (distance >= detailActivationDistance) {
             isDetailVisible = false;
             animator.SetBool("showText", false);
+            //animator.SetBool("showVideo", false);
+            // animator.Rebind();
         }
+    }
+
+    IEnumerator ShowVideoAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        animator.SetBool("showVideo", true);
     }
 
 
